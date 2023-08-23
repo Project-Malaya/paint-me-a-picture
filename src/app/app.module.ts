77216c0e07b0f468193bf2ad637c47db8a424ee1
airtorres <komsaira2015@gmail.com>
2023-08-24 00:23:40 +0800

    Add more content, create text component boiler plate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SloganComponent } from './components/slogan/slogan.component';
+import { TextComponent } from './components/text/text.component';
 
 @NgModule({
-  declarations: [AppComponent, SloganComponent],
+  declarations: [AppComponent, SloganComponent, TextComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -18,6 +20,7 @@ import { SloganComponent } from './components/slogan/slogan.component';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/text/text.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text',
+  templateUrl: './text.component.html',
+  styleUrls: ['./text.component.scss'],
+})
+export class TextComponent {
+  // size, color, style (bold, italic, etc?)
+}
